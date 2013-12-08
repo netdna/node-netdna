a8e509649b6864b02a3cd63d89d99a178782423d
Josh Mervine <jmervine@yp.com>
2013-12-07 17:50:21 -0800

    lint clean up

--- test/account_int.js
+++ test/account_int.js
@@ -6,53 +6,57 @@ var assert = require('assert');
 var time   = new Date().getTime();
 
 var netdna = require('../netdna')({
-    companyAlias: process.env.ALIAS
-  , consumerKey: process.env.KEY
-  , consumerSecret: process.env.SECRET
+    companyAlias: process.env.ALIAS,
+    consumerKey: process.env.KEY,
+    consumerSecret: process.env.SECRET
 });
 
 vows.describe('account').addBatch({
 
-  'get account info': {
-    topic: function() {
-      netdna.get('account.json', this.callback);
-    }, 'returns response': function(err, response) {
-      assert.isNull(err);
-      assert.isDefined(response);
-    }
-  },
+    'get account info': {
+        topic: function() {
+            netdna.get('account.json', this.callback);
+        },
+        'returns response': function(err, response) {
+            assert.isNull(err);
+            assert.isDefined(response);
+        }
+    },
 
-  'update account info': {
-    topic: function() {
-      netdna.put('account.json', {
-        name: time
-      }, this.callback);
-    }, 'returns response': function(err, response) {
-      assert.isNull(err);
-      assert.isDefined(response);
-      assert.equal(response.data.account.name, time);
-    }
-  },
+    'update account info': {
+        topic: function() {
+            netdna.put('account.json', {
+                name: time
+            }, this.callback);
+        },
+        'returns response': function(err, response) {
+            assert.isNull(err);
+            assert.isDefined(response);
+            assert.equal(response.data.account.name, time);
+        }
+    },
 
-  'get account address': {
-    topic: function() {
-      netdna.get('account.json/address', this.callback);
-    }, 'returns response': function(err, response) {
-      assert.isNull(err);
-      assert.isDefined(response);
-    }
-  },
+    'get account address': {
+        topic: function() {
+            netdna.get('account.json/address', this.callback);
+        },
+        'returns response': function(err, response) {
+            assert.isNull(err);
+            assert.isDefined(response);
+        }
+    },
 
-  'update account address': {
-    topic: function() {
-      netdna.put('account.json/address', {
-        street1: time
-      }, this.callback);
-    }, 'returns response': function(err, response) {
-      assert.isNull(err);
-      assert.isDefined(response);
-      assert.equal(response.data.address.street1, time);
+    'update account address': {
+        topic: function() {
+            netdna.put('account.json/address', {
+                street1: time
+            }, this.callback);
+        },
+        'returns response': function(err, response) {
+            assert.isNull(err);
+            assert.isDefined(response);
+            assert.equal(response.data.address.street1, time);
+        }
     }
-  }
 
 }).export(module, { error: false });
--- test/zones_int.js
+++ test/zones_int.js
@@ -5,34 +5,36 @@ var vows   = require('vows');
 var assert = require('assert');
 
 var netdna = require('../netdna')({
-    companyAlias: process.env.ALIAS
-  , consumerKey: process.env.KEY
-  , consumerSecret: process.env.SECRET
+    companyAlias: process.env.ALIAS,
+    consumerKey: process.env.KEY,
+    consumerSecret: process.env.SECRET
 });
 
 vows.describe('pull').addBatch({
 
-  'get pull': {
-    topic: function() {
-      netdna.get('zones/pull.json', this.callback);
-    }, 'returns response': function(err, response) {
-      assert.isNull(err);
-      assert.isDefined(response);
-      assert.isDefined(response.data);
-      assert.isDefined(response.data.pullzones);
-    }
-  },
+    'get pull': {
+        topic: function() {
+            netdna.get('zones/pull.json', this.callback);
+        },
+        'returns response': function(err, response) {
+            assert.isNull(err);
+            assert.isDefined(response);
+            assert.isDefined(response.data);
+            assert.isDefined(response.data.pullzones);
+        }
+    },
 
-  'post pull': {
-    topic: function() {
-      netdna.post('zones/pull.json', {
-          name: 'Testing-Local'
-        , url: 'http://test.com'
-      }, this.callback);
-    }, 'returns response': function(err, response) {
-      assert.isNull(err);
-      assert.isDefined(response);
+    'post pull': {
+        topic: function() {
+            netdna.post('zones/pull.json', {
+                name: 'Testing-Local',
+                url: 'http://test.com'
+            }, this.callback);
+        },
+        'returns response': function(err, response) {
+            assert.isNull(err);
+            assert.isDefined(response);
+        }
     }
-  }
 
 }).export(module, { error: false });
